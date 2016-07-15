a4463bad4df68652d9210bef9b69e3fb91038c8d
Hardslog <hardslog@gmail.com>
2016-07-15 16:20:06 -0400

    Add Hardslog light theme

--- tests/src/com/android/inputmethod/keyboard/KeyboardThemeTests.java
+++ tests/src/com/android/inputmethod/keyboard/KeyboardThemeTests.java
@@ -21,6 +21,7 @@ import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_KLP;
 import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_LXX_DARK;
 import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_LXX_LIGHT;
 import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_HS_DARK;
+import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_HS_LIGHT;
 import static com.android.inputmethod.keyboard.KeyboardTheme.THEME_ID_BLACKOUT;
 
 import static org.junit.Assert.assertEquals;
@@ -74,6 +75,7 @@ public class KeyboardThemeTests {
         case THEME_ID_LXX_LIGHT:
         case THEME_ID_LXX_DARK:
         case THEME_ID_HS_DARK:
+        case THEME_ID_HS_LIGHT:
         case THEME_ID_BLACKOUT:
             return true;
         default:
@@ -127,6 +129,7 @@ public class KeyboardThemeTests {
         assertKeyboardThemePreference(sdkVersion, THEME_ID_LXX_LIGHT, THEME_ID_LXX_LIGHT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_LXX_DARK, THEME_ID_LXX_DARK);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_HS_DARK, THEME_ID_HS_DARK);
+        assertKeyboardThemePreference(sdkVersion, THEME_ID_HS_LIGHT, THEME_ID_HS_LIGHT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_BLACKOUT, THEME_ID_BLACKOUT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_UNKNOWN, defaultThemeId);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_ILLEGAL, defaultThemeId);
@@ -150,6 +153,7 @@ public class KeyboardThemeTests {
         assertKeyboardThemePreference(sdkVersion, THEME_ID_LXX_LIGHT, THEME_ID_LXX_LIGHT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_LXX_DARK, THEME_ID_LXX_DARK);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_HS_DARK, THEME_ID_HS_DARK);
+        assertKeyboardThemePreference(sdkVersion, THEME_ID_HS_LIGHT, THEME_ID_HS_LIGHT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_BLACKOUT, THEME_ID_BLACKOUT);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_UNKNOWN, defaultThemeId);
         assertKeyboardThemePreference(sdkVersion, THEME_ID_ILLEGAL, defaultThemeId);
